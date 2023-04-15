6b8d0bd13f1a07d7f919772bdae67acf8cba5d78
Rafael Campello <rafael@citrine.la.asu.edu>
2023-04-15 00:53:30 -0700

    Added analysis of Omin and Opct

--- R/sensitivity.R
+++ R/sensitivity.R
@@ -18,7 +18,7 @@ ate.xbcf <- function(h,n)
     for (i in 1:length(h))
     {
         ## XBCF
-        fit.XBCFrd <- XBCF.rd(y, w, x, c, Owidth = h[i], Omin = Omin, Opct = Opct,
+        fit.XBCFrd <- XBCF.rd(y, w, x, c, Owidth = 0.1, Omin = Omin, Opct = Opct[i],
                               pcat_con = 0, pcat_mod = 0,
                               num_trees_mod = num_trees_mod, num_trees_con = num_trees_con,
                               num_cutpoints = num_cutpoints, num_sweeps = num_sweeps,
@@ -35,18 +35,18 @@ ate.xbcf <- function(h,n)
 mu <- function(W, X) return(0.1 * rowSums(W) + 1/(1+exp(-5*X)))
 tau <- function(W, X) return( sin(mu(W, X)) + 1)
 ### Plotting function
-plot.res <- function(Owidth,y,dist)
+plot.res <- function(Opct,y,dist)
 {
-    matplot(x=Owidth,y=y,type="b",lty=1,col=c("brown2","darkgoldenrod2","dodgerblue2"),main=dist,pch=16)
+    matplot(x=Opct,y=y,type="b",lty=1,col=c("brown2","darkgoldenrod2","dodgerblue2"),main=dist,pch=16)
     legend("topright",col=c("brown2","darkgoldenrod2","dodgerblue2"),legend=c(100,250,500),lty=1)
 }
 ## Estimation settings
 ### Owidth: overlap bandiwdth
 ### Omin: minimum number of observations inside overlap region for each leaf node
 ### Opct: If nb is the number of obs in node b, Opct*nb of them have to be inside overlap region
-Owidth        <- c(0.05,0.1,0.15,0.2)
-Omin          <- 10
-Opct          <- 0.7
+Owidth        <- 0.1
+Omin          <- 2
+Opct          <- c(0.3,0.5,0.7,0.9)
 num_trees_mod <- 10
 num_trees_con <- 10
 Nmin          <- 10
@@ -57,19 +57,11 @@ n <- c(100,250,500)
 p <- 2
 c <- 0 # Cutoff
 s <- 50
-obs <- list("Gaussian"=vector("list",length(Owidth)),
-            "Uniform"=vector("list",length(Owidth)))
-###
-for(i in 1:2)
-{
-    for (j in 1:length(Owidth))
-    {
-        obs[[i]][[j]] <- matrix(0,s,length(n))
-    }
-}
-###
-results <- list("Gaussian"=matrix(0,length(Owidth),length(n)),
-                "Uniform"=matrix(0,length(Owidth),length(n)))
+obs <- list("Gaussian"=vector("list",length(Opct)),
+            "Uniform"=vector("list",length(Opct)))
+
+results <- list("Gaussian"=matrix(0,length(Opct),length(n)),
+                "Uniform"=matrix(0,length(Opct),length(n)))
 for (h in 1:2)
 {
     if (h==1)
@@ -84,7 +76,7 @@ for (h in 1:2)
     for (i in 1:length(n))
     {
         num_cutpoints <- n[i]
-        temp.results <- matrix(0,length(Owidth),s)
+        temp.results <- matrix(0,length(Opct),s)
         for (j in 1:s)
         {
             print(paste0("Distibution of x: ",d))
@@ -95,30 +87,21 @@ for (h in 1:2)
             z <- x >= c
             y <- mu(w, x) + tau(w, x)*z + rnorm(n[i], 0, 0.1)
             true.ate <- mean(tau(w,0))
-            ate <- ate.xbcf(Owidth,n[i])
+            ate <- ate.xbcf(Opct,n[i])
             temp.results[,j] <- (ate-true.ate)^2
-            ## Count observations
-            for (k in 1:length(Owidth))
-            {
-                obs[[h]][[k]][j,i] <- sum((x>=-Owidth[k] & x<=Owidth[k]))/n[i]
-            }
-            ###
         }
         results[[h]][,i] <- rowMeans(temp.results)
     }
 }
 ###
-png("MSE_width.png")
+png("MSE_opct.png")
 par(mfrow=c(1,2))
-plot.res(Owidth,results$Gaussian,"Gaussian")
-plot.res(Owidth,results$Uniform,"Uniform")
+plot.res(Opct,results$Gaussian,"Gaussian")
+plot.res(Opct,results$Uniform,"Uniform")
 par(mfrow=c(1,1))
 dev.off()
 ###
 minMSE <- do.call("rbind",lapply(results,function(x) apply(x,2,min)))
 colnames(minMSE) <- n
-minWidth <- do.call("rbind",lapply(results,function(x) apply(x,2,function(x) Owidth[which(x==min(x))])))
-colnames(minWidth) <- n
-###
-obs <- lapply(obs,function(x) lapply(x,colMeans))
-obs <- 
+minOpct <- do.call("rbind",lapply(results,function(x) apply(x,2,function(x) Opct[which(x==min(x))])))
+colnames(minOpct) <- n
