af25144869aa128441307db74eb439841770aff0
Rafael Campello <rafael@citrine.la.asu.edu>
2023-08-04 13:18:07 -0700

    Added new partition example figures

--- R/partition_examples.R
+++ R/partition_examples.R
@@ -4,32 +4,38 @@ x <- runif(n, -1, 1)
 w <- runif(n, -1, 1)
 treated <- x >= 0
 
-par(mfrow = c(2, 2))
 cex = 0.8
 # First split, but opct is not satisfied
+png("Figures/bad_trees_1a.png")
 plot(x, w, pch = 16 + treated, cex = cex)
 abline(v = -0.25, lty = 2)
 abline(v = 0, lty = 2)
 abline(v = 0.25, lty = 2)
 abline(h = 0)
+dev.off()
 
 # A split that would not meat the first constraint 
 col <- (x > -0.25) & (x < 0) & (w > 0) & (w < 0.4)
+png("Figures/bad_trees_1b.png")
 plot(x, w, pch = 16 + treated, col = col + 1, cex = cex)
 abline(v = -0.25, lty = 2)
 abline(v = 0, lty = 2)
 abline(v = 0.25, lty = 2)
 abline(h = 0)
 abline(h = 0.25)
+dev.off()
 
 # Splits that are likely to happen to satisfy opct
+png("Figures/bad_trees_1c.png")
 plot(x, w, pch = 16 + treated, cex = cex)
 abline(v = -0.25, lty = 2)
 abline(v = 0, lty = 2)
 abline(v = 0.25, lty = 2)
 abline(h = 0)
 abline(v = -0.4)
+dev.off()
 
+png("Figures/good_tree_1.png")
 plot(x, w, pch = 16 + treated, cex = cex)
 abline(v = -0.25, lty = 2)
 abline(v = 0, lty = 2)
@@ -37,3 +43,4 @@ abline(v = 0.25, lty = 2)
 abline(h = 0)
 abline(v = -0.4)
 abline(v = 0.4)
+dev.off()
