4d75da55951d8e825109d38a19c84b7a1dab4fea
Rafael Campello <rafael@citrine.la.asu.edu>
2023-08-08 10:44:23 -0700

    Added correct results for BART models

--- R/simulation.R
+++ R/simulation.R
@@ -29,135 +29,132 @@ if (Sys.info()["sysname"] == "Linux")
     num_sweeps    <- 150
     burnin        <- 50
     p_categorical <- 1
-##     ## BART-RDD
-##     fit <- function(s,n,m,xi,nu,kappa)
-##     {
-##         foreach(i=1:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
-##             {
-##                 print("BART-RDD simulations")
-##                 print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
-##                 print(paste0("Simulation ",i))
-##                 data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",i))
-##                 t0 <- Sys.time()
-##                 fit <- XBCF.rd(data$Y, data$W, data$X, c,
-##                                Owidth = h, Omin = Omin, Opct = Opct,
-##                                num_trees_mod = m, num_trees_con = m,
-##                                num_cutpoints = n,
-##                                num_sweeps = num_sweeps,
-##                                burnin = burnin, Nmin = Nmin,
-##                                p_categorical_con = p_categorical,
-##                                p_categorical_mod = p_categorical,
-##                                tau_con = 2*var(data$Y)/m,
-##                                tau_mod = 0.5*var(data$Y)/m, parallel=F)
-##                 test <- -h<=data$X & data$X<=h
-##                 pred <- predict.XBCFrd(fit,data$W[test,],rep(0,sum(test)))
-##                 pred <- pred$tau.adj[,(burnin+1):num_sweeps]
-##                 t1 <- Sys.time()
-##                 dt <- difftime(t1,t0)
-##                 return(list(pred=pred,dt=dt))
-##             }
-##     }
-## ###
-##     for (i in sample)
-##     {
-##         for (j in model)
-##         {
-##             for (k in xi)
-##             {
-##                 for (l in nu)
-##                 {
-##                     for (m in kappa)
-##                     {
-##                         bart.rdd <- fit(s,i,j,k,l,m)
-##                         saveRDS(bart.rdd,paste0("Results/bart_rdd_",i,"_",j,"_",k,"_",l,"_",m,".rds"))
-##                     }
-##                 }
-##             }
-##         }
-##     }
-##     ## CCT
-##     fit <- function(s,n,m,xi,nu,kappa)
-##     {
-##         foreach(i=1:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
-##             {
-##                 print("CCT simulations")
-##                 print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
-##                 print(paste0("Simulation ",i))
-##                 data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",i))
-##                 t0 <- Sys.time()
-##                 fit <- rdrobust(data$Y,data$X,0,covs=data$W)
-##                 t1 <- Sys.time()
-##                 dt <- difftime(t1,t0)
-##                 pred <- c(fit$coef[1],fit$ci[3,])
-##                 return(list(pred=pred,dt=dt))
-##             }
-##     }
-## ###
-##     for (i in sample)
-##     {
-##         for (j in model)
-##         {
-##             for (k in xi)
-##             {
-##                 for (l in nu)
-##                 {
-##                     for (m in kappa)
-##                     {
-##                         cct <- fit(s,i,j,k,l,m)
-##                         saveRDS(cct,paste0("Results/cct_",i,"_",j,"_",k,"_",l,"_",m,".rds"))
-##                     }
-##                 }
-##             }
-##         }
-##     }
-    ## KR
-    fit <- function(s,n,m,xi,nu,kappa)
+    ## BART-RDD
+    fit <- function(s,n,Model,xi,nu,kappa)
     {
         foreach(i=1:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
             {
-                print("KR simulations")
-                print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
+                print("BART-RDD simulations")
+                print(paste0("N=",n,"; Model=",Model,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
                 print(paste0("Simulation ",i))
-                data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",i))
+                data <- readRDS(paste0("Data/DGP_",n,"_",Model,"_",xi,"_",nu,"_",kappa,"_",i))
                 t0 <- Sys.time()
-                w1 <- fourier_basis(matrix(data$X),4)
-                w_HighDim <- cbind(data$W,interaction_terms(data$W),w1,interaction_terms(w1))
-                ## Estimation
-                fit  <- HighDim_rd(data$Y,data$X,w_HighDim,
-                                   tpc="CV",rd="robust")
+                fit <- XBCF.rd(data$Y, data$W, data$X, c,
+                               Owidth = h, Omin = Omin, Opct = Opct,
+                               num_trees_mod = m, num_trees_con = m,
+                               num_cutpoints = n,
+                               num_sweeps = num_sweeps,
+                               burnin = burnin, Nmin = Nmin,
+                               p_categorical_con = p_categorical,
+                               p_categorical_mod = p_categorical,
+                               tau_con = 2*var(data$Y)/m,
+                               tau_mod = 0.5*var(data$Y)/m, parallel=F)
+                test <- -h<=data$X & data$X<=h
+                pred <- predict.XBCFrd(fit,data$W[test,],rep(0,sum(test)))
+                pred <- pred$tau.adj[,(burnin+1):num_sweeps]
                 t1 <- Sys.time()
                 dt <- difftime(t1,t0)
-                pred <- c(fit$rd[["Estimate"]][,"tau.bc"],fit$rd[["ci"]]["Robust",])
                 return(list(pred=pred,dt=dt))
             }
     }
 ###
-    for (i in sample)
+    for (i in model)
     {
-        for (j in model)
+        for (j in xi)
         {
-            for (k in xi)
+            for (k in nu)
             {
-                for (l in nu)
+                for (l in kappa)
                 {
-                    for (m in kappa)
-                    {
-                        kr <- fit(s,i,j,k,l,m)
-                        saveRDS(kr,paste0("Results/kr_",i,"_",j,"_",k,"_",l,"_",m,".rds"))
-                    }
+                    bart.rdd <- fit(s,500,i,j,k,l)
+                    saveRDS(bart.rdd,paste0("Results/bart_rdd_500_",i,"_",j,"_",k,"_",l,".rds"))
                 }
             }
         }
     }
+    ##     ## CCT
+    ##     fit <- function(s,n,m,xi,nu,kappa)
+    ##     {
+    ##         foreach(i=1:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
+    ##             {
+    ##                 print("CCT simulations")
+    ##                 print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
+    ##                 print(paste0("Simulation ",i))
+    ##                 data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",i))
+    ##                 t0 <- Sys.time()
+    ##                 fit <- rdrobust(data$Y,data$X,0,covs=data$W)
+    ##                 t1 <- Sys.time()
+    ##                 dt <- difftime(t1,t0)
+    ##                 pred <- c(fit$coef[1],fit$ci[3,])
+    ##                 return(list(pred=pred,dt=dt))
+    ##             }
+    ##     }
+    ## ###
+    ##     for (i in sample)
+    ##     {
+    ##         for (j in model)
+    ##         {
+    ##             for (k in xi)
+    ##             {
+    ##                 for (l in nu)
+    ##                 {
+    ##                     for (m in kappa)
+    ##                     {
+    ##                         cct <- fit(s,i,j,k,l,m)
+    ##                         saveRDS(cct,paste0("Results/cct_",i,"_",j,"_",k,"_",l,"_",m,".rds"))
+    ##                     }
+    ##                 }
+    ##             }
+    ##         }
+    ##     }
+    ##     ## KR
+    ##     fit <- function(s,n,m,xi,nu,kappa)
+    ##     {
+    ##         foreach(i=1:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
+    ##             {
+    ##                 print("KR simulations")
+    ##                 print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
+    ##                 print(paste0("Simulation ",i))
+    ##                 data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",i))
+    ##                 t0 <- Sys.time()
+    ##                 w1 <- fourier_basis(matrix(data$X),4)
+    ##                 w_HighDim <- cbind(data$W,interaction_terms(data$W),w1,interaction_terms(w1))
+    ##                 ## Estimation
+    ##                 fit  <- HighDim_rd(data$Y,data$X,w_HighDim,
+    ##                                    tpc="CV",rd="robust")
+    ##                 t1 <- Sys.time()
+    ##                 dt <- difftime(t1,t0)
+    ##                 pred <- c(fit$rd[["Estimate"]][,"tau.bc"],fit$rd[["ci"]]["Robust",])
+    ##                 return(list(pred=pred,dt=dt))
+    ##             }
+    ##     }
+    ## ###
+    ##     for (i in sample)
+    ##     {
+    ##         for (j in model)
+    ##         {
+    ##             for (k in xi)
+    ##             {
+    ##                 for (l in nu)
+    ##                 {
+    ##                     for (m in kappa)
+    ##                     {
+    ##                         kr <- fit(s,i,j,k,l,m)
+    ##                         saveRDS(kr,paste0("Results/kr_",i,"_",j,"_",k,"_",l,"_",m,".rds"))
+    ##                     }
+    ##                 }
+    ##             }
+    ##         }
+    ##     }
     ## BART1
-    fit <- function(s,n,m,xi,nu,kappa)
+    fit <- function(s,n,Model,xi,nu,kappa)
     {
         foreach(i=1:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
             {
                 print("BART1 simulations")
-                print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
+                print(paste0("N=",n,"; Model=",Model,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
                 print(paste0("Simulation ",i))
-                data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",i))
+                data <- readRDS(paste0("Data/DGP_",n,"_",Model,"_",xi,"_",nu,"_",kappa,"_",i))
                 X <- cbind(data$X,data$W,data$Z)
                 t0 <- Sys.time()
                 fit <- XBART(data$Y, X, num_trees = m,
@@ -178,32 +175,29 @@ if (Sys.info()["sysname"] == "Linux")
             }
     }
 ###
-    for (i in sample)
+    for (i in model)
     {
-        for (j in model)
+        for (j in xi)
         {
-            for (k in xi)
+            for (k in nu)
             {
-                for (l in nu)
+                for (l in kappa)
                 {
-                    for (m in kappa)
-                    {
-                        bart1 <- fit(s,i,j,k,l,m)
-                        saveRDS(bart1,paste0("Results/bart1_",i,"_",j,"_",k,"_",l,"_",m,".rds"))
-                    }
+                    bart1 <- fit(s,500,i,j,k,l)
+                    saveRDS(bart1,paste0("Results/bart1_500_",i,"_",j,"_",k,"_",l,".rds"))
                 }
             }
         }
     }
     ## BART2
-    fit <- function(s,n,m,xi,nu,kappa)
+    fit <- function(s,n,Model,xi,nu,kappa)
     {
         foreach(i=1:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
             {
                 print("BART2 simulations")
-                print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
+                print(paste0("N=",n,"; Model=",Model,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
                 print(paste0("Simulation ",i))
-                data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",i))
+                data <- readRDS(paste0("Data/DGP_",n,"_",Model,"_",xi,"_",nu,"_",kappa,"_",i))
                 X <- cbind(data$X,data$W)
                 t0 <- Sys.time()
                 fit0 <- XBART(data$Y[!data$Z], X[!data$Z,],
@@ -237,32 +231,29 @@ if (Sys.info()["sysname"] == "Linux")
             }
     }
 ###
-    for (i in sample)
+    for (i in model)
     {
-        for (j in model)
+        for (j in xi)
         {
-            for (k in xi)
+            for (k in nu)
             {
-                for (l in nu)
+                for (l in kappa)
                 {
-                    for (m in kappa)
-                    {
-                        bart2 <- fit(s,i,j,k,l,m)
-                        saveRDS(bart2,paste0("Results/bart2_",i,"_",j,"_",k,"_",l,"_",m,".rds"))
-                    }
+                    bart2 <- fit(s,500,i,j,k,l)
+                    saveRDS(bart2,paste0("Results/bart2_500_",i,"_",j,"_",k,"_",l,".rds"))
                 }
             }
         }
     }
     ## BCF
-    fit <- function(s,n,m,xi,nu,kappa)
+    fit <- function(s,n,Model,xi,nu,kappa)
     {
         foreach(i=1:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
             {
                 print("BCF simulations")
-                print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
+                print(paste0("N=",n,"; Model=",Model,"; xi=",xi,"; nu=",nu,"; kappa=",kappa))
                 print(paste0("Simulation ",i))
-                data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",i))
+                data <- readRDS(paste0("Data/DGP_",n,"_",Model,"_",xi,"_",nu,"_",kappa,"_",i))
                 X <- cbind(data$X,data$W)
                 t0 <- Sys.time()
                 fit <- XBCF.discrete(data$Y, data$Z, X_con=X, X_mod=X,
@@ -288,26 +279,23 @@ if (Sys.info()["sysname"] == "Linux")
             }
     }
 ###
-    for (i in sample)
+    for (i in model)
     {
-        for (j in model)
+        for (j in xi)
         {
-            for (k in xi)
+            for (k in nu)
             {
-                for (l in nu)
+                for (l in kappa)
                 {
-                    for (m in kappa)
-                    {
-                        bcf <- fit(s,i,j,k,l,m)
-                        saveRDS(bcf,paste0("Results/bcf_",i,"_",j,"_",k,"_",l,"_",m,".rds"))
-                    }
+                    bcf <- fit(s,500,i,j,k,l)
+                    saveRDS(bcf,paste0("Results/bcf_500_",i,"_",j,"_",k,"_",l,".rds"))
                 }
             }
         }
     }
 } else
 {
-  setwd("~/Git/XBCF-RDD/")
+    setwd("~/Git/XBCF-RDD/")
     ## CGS
     install.packages("http://apps.olin.wustl.edu/faculty/chib/rpackages/rdd/bayesrdd_1.0.zip",repo=NULL,source=T)
     library(bayesrdd)
