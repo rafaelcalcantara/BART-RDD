66d99cf68461855a00f1d943adcd670d84f97f4c
Rafael Campello <rafael@citrine.la.asu.edu>
2023-08-10 13:31:09 -0700

    Plot rmse_h results with finer grid

--- R/hyperparameters.R
+++ R/hyperparameters.R
@@ -18,74 +18,13 @@ Nmin          <- 10
 num_sweeps    <- 120
 burnin        <- 20
 p_categorical <- 1
-## h
-fit <- function(s,n,m,xi,nu,kappa)
-{
-    foreach(i=0:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
-        {
-            h <- 0.05 + (i-1)/100
-            print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa,"; h=",h))
-            mse <- rep(0,5)
-            for (sample in 1:5)
-            {
-                data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",sample))
-                fit <- XBCF.rd(data$Y, data$W, data$X, c,
-                               Owidth = h, Omin = Omin, Opct = Opct,
-                               num_trees_mod = ntrees,
-                               num_trees_con = ntrees,
-                               num_cutpoints = n,
-                               num_sweeps = num_sweeps,
-                               burnin = burnin, Nmin = Nmin,
-                               p_categorical_con = p_categorical,
-                               p_categorical_mod = p_categorical,
-                               tau_con = 2*var(data$Y)/ntrees,
-                               tau_mod = 0.5*var(data$Y)/ntrees, parallel=F)
-                test <- -h<=data$X & data$X<=h
-                pred <- predict.XBCFrd(fit,data$W[test,],rep(0,sum(test)))
-                pred <- pred$tau.adj[,(burnin+1):num_sweeps]
-                pred <- colMeans(pred)
-                mse[sample] <- (mean(pred)-xi)^2
-            }
-            return(sqrt(mean(mse)))
-        }
-}
-###
-results.h <- vector("list",32)
-index <- 0
-for (j in model)
-{
-    for (k in xi)
-    {
-        for (l in nu)
-        {
-            for (m in kappa)
-            {
-                index <- index+1
-                results.h[[index]] <- fit(11,500,j,k,l,m)
-            }
-        }
-    }
-}
-###
-saveRDS(results.h,"Tables/rmse_h.rds")
-rmse <- apply(do.call("rbind",lapply(results.h,unlist)),2,function(x) mean(sqrt(x)))
-rmse <- data.frame(h=seq(0.05,0.15,0.01),RMSE=rmse)
-###
-yticks <- seq(min(rmse$RMSE),max(rmse$RMSE),(-min(rmse$RMSE)+max(rmse$RMSE))/5)
-png("Figures/rmse_h.png")
-plot(rmse,type="b",col="blue",pch=19,cex=0.75,axes=F)
-axis(1,at=rmse$h,labels=F)
-text(x=rmse$h,par("usr")[3]-0.002,labels=rmse$h,pos=1,xpd=T,cex=0.75)
-axis(2,at=yticks,labels=F)
-text(y=yticks,par("usr")[1]-0.002,labels=round(yticks,2),pos=2,xpd=T,cex=0.75)
-dev.off()
-## ## Omin
+## ## h
 ## fit <- function(s,n,m,xi,nu,kappa)
 ## {
 ##     foreach(i=1:s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
 ##         {
-##             Omin <- i*(i==1) + 5*(i-1)*(i>1)
-##             print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa,"; Omin=",Omin))
+##             h <- 0.05 + (i-1)/100
+##             print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa,"; h=",h))
 ##             mse <- rep(0,5)
 ##             for (sample in 1:5)
 ##             {
@@ -100,8 +39,7 @@ dev.off()
 ##                                p_categorical_con = p_categorical,
 ##                                p_categorical_mod = p_categorical,
 ##                                tau_con = 2*var(data$Y)/ntrees,
-##                                tau_mod = 0.5*var(data$Y)/ntrees,
-##                                parallel=F)
+##                                tau_mod = 0.5*var(data$Y)/ntrees, parallel=F)
 ##                 test <- -h<=data$X & data$X<=h
 ##                 pred <- predict.XBCFrd(fit,data$W[test,],rep(0,sum(test)))
 ##                 pred <- pred$tau.adj[,(burnin+1):num_sweeps]
@@ -112,7 +50,7 @@ dev.off()
 ##         }
 ## }
 ## ###
-## results.omin <- vector("list",32)
+## results.h <- vector("list",32)
 ## index <- 0
 ## for (j in model)
 ## {
@@ -123,22 +61,85 @@ dev.off()
 ##             for (m in kappa)
 ##             {
 ##                 index <- index+1
-##                 results.omin[[index]] <- fit(5,500,j,k,l,m)
+##                 results.h[[index]] <- fit(11,500,j,k,l,m)
 ##             }
 ##         }
 ##     }
 ## }
 ## ###
-## saveRDS(results.omin,"Tables/rmse_omin.rds")
-## rmse <- apply(do.call("rbind",lapply(results.omin,unlist)),2,function(x) mean(sqrt(x)))
-## rmse <- data.frame(N=c(1,5,10,15,20),RMSE=rmse)
+## saveRDS(results.h,"Tables/rmse_h.rds")
+## rmse <- apply(do.call("rbind",lapply(results.h,unlist)),2,function(x) mean(sqrt(x)))
+## rmse <- data.frame(h=seq(0.05,0.15,0.01),RMSE=rmse)
 ## ###
 ## yticks <- seq(min(rmse$RMSE),max(rmse$RMSE),(-min(rmse$RMSE)+max(rmse$RMSE))/5)
-## png("Figures/rmse_omin.png")
+## png("Figures/rmse_h.png")
 ## plot(rmse,type="b",col="blue",pch=19,cex=0.75,axes=F)
-## axis(1,at=rmse$N,labels=F)
-## text(x=rmse$N,par("usr")[3]-0.002,labels=rmse$N,pos=1,xpd=T,cex=0.75)
+## axis(1,at=rmse$h,labels=F)
+## text(x=rmse$h,par("usr")[3]-0.002,labels=rmse$h,pos=1,xpd=T,cex=0.75)
 ## axis(2,at=yticks,labels=F)
 ## text(y=yticks,par("usr")[1]-0.002,labels=round(yticks,2),pos=2,xpd=T,cex=0.75)
 ## dev.off()
+## Omin
+fit <- function(s,n,m,xi,nu,kappa)
+{
+    foreach(i=s,.multicombine=T,.export=c("n","c","Omin","h","Opct","m","n","num_sweeps","burnin","Nmin","p_categorical")) %dopar%
+        {
+            Omin <- i
+            print(paste0("N=",n,"; Model=",m,"; xi=",xi,"; nu=",nu,"; kappa=",kappa,"; Omin=",Omin))
+            mse <- rep(0,5)
+            for (sample in 1:5)
+            {
+                data <- readRDS(paste0("Data/DGP_",n,"_",m,"_",xi,"_",nu,"_",kappa,"_",sample))
+                fit <- XBCF.rd(data$Y, data$W, data$X, c,
+                               Owidth = h, Omin = Omin, Opct = Opct,
+                               num_trees_mod = ntrees,
+                               num_trees_con = ntrees,
+                               num_cutpoints = n,
+                               num_sweeps = num_sweeps,
+                               burnin = burnin, Nmin = Nmin,
+                               p_categorical_con = p_categorical,
+                               p_categorical_mod = p_categorical,
+                               tau_con = 2*var(data$Y)/ntrees,
+                               tau_mod = 0.5*var(data$Y)/ntrees,
+                               parallel=F)
+                test <- -h<=data$X & data$X<=h
+                pred <- predict.XBCFrd(fit,data$W[test,],rep(0,sum(test)))
+                pred <- pred$tau.adj[,(burnin+1):num_sweeps]
+                pred <- colMeans(pred)
+                mse[sample] <- (mean(pred)-xi)^2
+            }
+            return(sqrt(mean(mse)))
+        }
+}
+###
+grid <- c(1,2,5,7,10)
+results.omin <- vector("list",32)
+index <- 0
+for (j in model)
+{
+    for (k in xi)
+    {
+        for (l in nu)
+        {
+            for (m in kappa)
+            {
+                index <- index+1
+                results.omin[[index]] <- fit(grid,500,j,k,l,m)
+            }
+        }
+    }
+}
+###
+saveRDS(results.omin,"Tables/rmse_omin.rds")
+rmse <- apply(do.call("rbind",lapply(results.omin,unlist)),2,function(x) mean(sqrt(x)))
+rmse <- data.frame(N=grid,RMSE=rmse)
+###
+yticks <- seq(min(rmse$RMSE),max(rmse$RMSE),(-min(rmse$RMSE)+max(rmse$RMSE))/5)
+png("Figures/rmse_omin.png")
+plot(rmse,type="b",col="blue",pch=19,cex=0.75,axes=F)
+axis(1,at=rmse$N,labels=F)
+text(x=rmse$N,par("usr")[3]-0.002,labels=rmse$N,pos=1,xpd=T,cex=0.75)
+axis(2,at=yticks,labels=F)
+text(y=yticks,par("usr")[1]-0.002,labels=round(yticks,2),pos=2,xpd=T,cex=0.75)
+dev.off()
 print("Simulation ended!")
