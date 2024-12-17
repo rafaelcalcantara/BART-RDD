7d60959a258835b863e0a82baab72c961a02355f
Rafael Alcantara <rafaelcampalc@gmail.com>
2024-12-17 15:39:35 -0600

    updated no_cores for cluster in prior predictive script

--- R/prior_predictive.R
+++ R/prior_predictive.R
@@ -4,8 +4,8 @@ library(doParallel)
 library(XBART)
 library(lattice)
 ### Parallelization
-no_cores <- detectCores()-1
-# no_cores <- 125
+# no_cores <- detectCores()-1
+no_cores <- 125
 ## Fitting function for prior predictive
 fit <- function(i)
 {
