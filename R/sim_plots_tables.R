7421cf988a3d111b6a1dd34e75879f42d4b1a772
Rafael Alcantara <rafaelcampalc@gmail.com>
2024-12-27 16:50:09 -0600

    Changed rho grid

--- R/sim_plots_tables.R
+++ R/sim_plots_tables.R
@@ -22,7 +22,7 @@ for (i in c(500,1000,1500))
   #      lty = 2, col = "gray", lwd = 1)
 }
 plot.new()
-legend("center",col=1:3,legend=c("0.45","0.9"),
+legend("center",col=1:3,legend=c("-0.9","0.9"),
        title=bquote(rho),ncol=2,lty=1,pch=19,cex=0.75,lwd=2)
 ###
 # sample <- 1
--- R/w_given_x.R
+++ R/w_given_x.R
@@ -39,12 +39,12 @@ c <- 0
 n <- 500
 ate <- 1
 ####
-rho <- 0.8
+rho <- 0.4
 u1 <- rnorm(n)
 u2 <- rnorm(n,rho*u1,sqrt(1-rho^2))
 u <- pnorm(cbind(u1,u2))
-x <- 2*qbeta(u[,1],2,4)-1
-w <- 2*qbeta(u[,2],2,5)-1
+x <- 2*qbeta(u[,1],5,1)-1
+w <- 2*qbeta(u[,2],1,5)-1
 c <- 0
 z <- as.numeric(x>=c)
 y <- mu(x,w) + tau(x,c,w,ate)*z + rnorm(n)
