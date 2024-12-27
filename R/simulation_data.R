abf5f14b9242451e3132fe4ef6f7f03b0344a795
Rafael Alcantara <rafaelcampalc@gmail.com>
2024-12-27 15:40:31 -0600

    Changed tau.w

--- R/simulation_data.R
+++ R/simulation_data.R
@@ -12,7 +12,7 @@ if (length(list.files("Data"))!=0) ## Clean up folder
 mu0.x <- function(x) 1.5*x^5 - 0.6*x^3 + 0.25*x + 0.5
 mu0.w <- function(w) -15*sin(w)
 tau0.x <- function(x,c) log(x-c+1)
-tau0.w <- function(w) sin(1.5*pi*w)
+tau0.w <- function(w) sin(0.5*pi*w)
 mu <- function(x,w) {
   mu.w <- mu0.w(w)
   mu0.x(x) + sqrt(3)*mu.w/sd(mu.w)
