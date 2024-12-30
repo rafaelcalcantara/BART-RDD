45b0d115128c47ac8564a2edba0d0a30f62dd2cf
Rafael Alcantara <rafaelcampalc@gmail.com>
2024-12-30 15:55:39 -0600

    Update simulation_data.R

--- R/simulation_data.R
+++ R/simulation_data.R
@@ -9,7 +9,7 @@ if (length(list.files("Data"))!=0) ## Clean up folder
   for (i in files) file.remove(i)
 }
 ### Functions
-mu0.x <- function(x) x + 0.5
+mu0.x <- function(x) 1.5*x^5 - 0.6*x^3 + 0.25*x + 0.5
 mu0.w <- function(w) -15*sin(w)
 tau0.x <- function(x,c) x
 tau0.w <- function(w) sin(3*pi*w)
