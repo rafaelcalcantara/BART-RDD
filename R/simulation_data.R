eb84a45f6ae3a1dc0ae0509b8d839f539099d716
Rafael Alcantara <rafaelcampalc@gmail.com>
2025-01-01 13:17:33 -0600

    Update simulation_data.R

--- R/simulation_data.R
+++ R/simulation_data.R
@@ -9,10 +9,10 @@ if (length(list.files("Data"))!=0) ## Clean up folder
   for (i in files) file.remove(i)
 }
 ### Functions
-mu0.x <- function(x) -3*x^5 + 0.5*x^3 + 0.1*x^2 - 0.1*x + 1.72
+mu0.x <- function(x) -0.03*x^5 + 0.5*x^3 + 0.1*x^2 - 0.1*x + 1.72
 mu0.w <- function(w) sin(w)
-tau0.x <- function(x,c) 1/(1-exp(-4-x)) - 1/(1-exp(-4))
-tau0.w <- function(w) sin(w)
+tau0.x <- function(x,c) 1/(1-exp(-4-x)) - 1.02
+tau0.w <- function(w) cos(w)
 mu <- function(x,w) mu0.x(x) + mu0.w(w)
 tau <- function(x,c,w,ate) tau0.x(x,c) + tau0.w(w) + ate
 h.grid <- function(x,c,grid)
@@ -46,7 +46,7 @@ pts_in_window <- 75
 s <- 1000
 c <- 0
 ate <- 1
-sig_error <- 0.65
+sig_error <- 1
 ind <- 0
 params <- expand.grid(N,rho)
 gen.data <- function(ind)
