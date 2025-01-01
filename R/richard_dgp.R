52174766d027a60592045d44297e6c12d85e512e
Rafael Alcantara <rafaelcampalc@gmail.com>
2025-01-01 13:34:44 -0600

    Update richard_dgp.R

--- R/richard_dgp.R
+++ R/richard_dgp.R
@@ -3,14 +3,14 @@ set.seed(7)
 #### Steep mu.x
 # mu0.x <- function(x) 0.05*x^5 - 0.6*x^3 + 1.5*x^2 + 0.25*x + 0.5
 #### Not steep mu.x
-mu0.x <- function(x,k) -0.3*x^5 + 0.5*x^3 + 0.1*x^2 - 0.1*x + 1.72
-mu0.w <- function(w) 3*sin(w)
+mu0.x <- function(x) -0.03*x^5 + 0.5*x^3 + 0.1*x^2 - 0.1*x + 1.72
+mu0.w <- function(w) 0.4*sin(w)
 #### Steep tau.x
 # tau0.x <- function(x,c) sin(0.5*pi*x)
 #### Not steep tau.x
-tau0.x <- function(x,c) 1/(1-exp(-4-x)) - 1/(1-exp(-4))
-tau0.w <- function(w) 3*sin(w)
-mu <- function(x,w,k) mu0.x(x,k) + mu0.w(w)
+tau0.x <- function(x,c) 1/(1-exp(-4-x)) - 1.02
+tau0.w <- function(w) 0.6*cos(w)
+mu <- function(x,w,k) mu0.x(x) + mu0.w(w)
 tau <- function(x,c,w,ate) tau0.x(x,c) + tau0.w(w) + ate
 h.grid <- function(x,c,grid)
 {
@@ -42,7 +42,7 @@ n <- 1500
 # mtemp <- (x+1)/2
 # stemp <- 20
 # w <- rbeta(n,mtemp*stemp,(1-mtemp)*stemp)-0.5
-rho <- 0
+rho <- 0.5
 k <- 0.2
 u1 <- rnorm(n)
 u2 <- rnorm(n,rho*u1,sqrt(1-rho^2))
@@ -55,23 +55,23 @@ w <- qnorm(u[,2],0,sqrt(1))
 # w <- rnorm(n,rho*x,sqrt(1-rho^2))
 test <- c-h.grid(x,c,30) <= x & x <= c+h.grid(x,c,30)
 par(mfrow=c(1,2))
-plot(x,mu(x,w,k))
+plot(x,mu(x,w))
 plot(x,tau(x,c,w,ate))
 cor(x,w)
 summary(w[test])
 var(w)
-sd(mu(c,w,k))
+sd(mu(c,w))
 sd(tau(c,c,w,ate))
-sd(mu(c,w,k))/sd(tau(c,c,w,ate))
+sd(mu(c,w))/sd(tau(c,c,w,ate))
 ### Checking functions near x=c
-plot(x[test],mu(x,w,k)[test])
+plot(x[test],mu(x,w)[test])
 plot(x[test],tau(x,c,w,ate)[test])
 ### Checking functions at x=c
-plot(w[test],mu(c,w,k)[test])
+plot(w[test],mu(c,w)[test])
 plot(w[test],tau(c,c,w,ate)[test])
 ## Checking y
 z <- as.numeric(x>=c)
-y <- mu(x,w,k) + tau(x,c,w,ate)*z + rnorm(n,0,sqrt(0.01))
+y <- mu(x,w) + tau(x,c,w,ate)*z + rnorm(n,0,sqrt(1))
 par(mfrow=c(1,1))
 plot(x,y,col=z+1,pch=19,bty="l")
 abline(v=c,lty=2)
