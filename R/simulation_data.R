491770f42cc181200080cc903513ae9031aa8fec
Rafael Alcantara <rafaelcampalc@gmail.com>
2025-01-01 18:25:26 -0600

    Update simulation_data.R

--- R/simulation_data.R
+++ R/simulation_data.R
@@ -12,7 +12,7 @@ if (length(list.files("Data"))!=0) ## Clean up folder
 mu0.x <- function(x) -0.03*x^5 + 0.5*x^3 + 0.1*x^2 - 0.1*x + 1.72
 mu0.w <- function(w) cos(w)
 tau0.x <- function(x,c) 1/(1-exp(-4-x)) - 1.02
-tau0.w <- function(w) sin(w)
+tau0.w <- function(w) 0.7*sin(w)
 mu <- function(x,w) mu0.x(x) + mu0.w(w)
 tau <- function(x,c,w,ate) tau0.x(x,c) + tau0.w(w) + ate
 h.grid <- function(x,c,grid)
