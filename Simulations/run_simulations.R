df6ad09cc760bd7a84eb33ec73bf0fb96992c65d
Rafael Alcantara <rafaelcampalc@gmail.com>
2025-01-27 22:18:56 -0600

    Update run_simulations.R

--- Simulations/run_simulations.R
+++ Simulations/run_simulations.R
@@ -2,4 +2,4 @@ cluster <- TRUE
 n <- 4000
 s <- 100
 source("generate_master_scripts.R")
-for (i in 1:1) source(paste0("simulation_master",i,".R"))
+for (i in 2:14) source(paste0("simulation_master",i,".R"))
