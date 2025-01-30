2fafee77d9a639c65bc0f3c43caf9c1a413296a2
Rafael Alcantara <rafaelcampalc@gmail.com>
2025-01-29 19:40:37 -0600

    Smaller run of all DGPs

--- Simulations/simulation_master7.R
+++ Simulations/simulation_master7.R
@@ -3,22 +3,22 @@ no_cores <- 10
 c <- 0
 Owidth <- 0.1
 ## DGP parameters
-k1 <- 2 # larger gives more weight to x relative to w in mu(x,w)
-k2 <- 2 # relative size of sd(tau(w, x = c)) to sd(mu(w, x =c))
+k1 <- 1 # larger gives more weight to x relative to w in mu(x,w)
+k2 <- 1 # relative size of sd(tau(w, x = c)) to sd(mu(w, x =c))
 k3 <- 0 # separable in x and w or not, 0 is separable
 k4 <- 0.1 # noise relative to sd(mu)
-k5 <- 0.25 # size of the ate as a multiple of the residual standard deviation
-p <- 5
+k5 <- 0 # size of the ate as a multiple of the residual standard deviation
+p <- 4
 rho <- 0
 ## Sample size
-n <- 500
+n <- 4000
 ## Simulation reps
-s <- 10
+s <- 100
 ## Which models to run
 models <- c('leaf.rdd','tbart','sbart','polynomial')
 ## Generate data
 ### Identifier of DGP configuration for names of data and results files
-dgp <- 'k1_2_k2_2_k3_0_k4_0.1_k5_0.25_p_5_rho_0'
+dgp <- 'k1_1_k2_1_k3_0_k4_0.1_k5_0_p_4_rho_0'
 ## Generate the fixed w used in all settings with p features
 set.seed(007)
 K <- 2*toeplitz(seq(1,0,length.out = p))
@@ -27,4 +27,11 @@ write.table(w, paste0("Data/w_",p,".csv"), row.names = FALSE, col.names = FALSE,
 ## Create log folders for current DGP
 if (dgp %in% list.files("Logs") == FALSE) dir.create(paste0("Logs/",dgp))
 ## Estimation
-source("simulation_estimation_local.R")
+batch <- c(1,1)
+for (i in 0:99) { # Running simulations in batches of 10
+s0 <- batch[1]+i*1
+s1 <- batch[2]+i*1
+batch.args <- paste(s0,s1,dgp,n,c,Owidth,paste(k1,k2,k3,k4,k5,p,rho, colapse=" "),collapse=" ")
+batch.script <- paste0("nice Rscript --verbose simulation_estimation_cluster.R ",batch.args, " > Logs/", dgp, "/outputFile_batch", i+1, "_", dgp, ".Rout 2> Logs/", dgp, "/errorFile_batch", i+1, "_", dgp, ".Rout"," &")
+system(batch.script)
+}
