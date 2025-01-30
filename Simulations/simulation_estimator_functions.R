f5263aafd067214bb42480f6799e1aa3508a19b2
Rafael Alcantara <rafaelcampalc@gmail.com>
2025-01-29 22:05:04 -0600

    Update simulation_estimator_functions.R

--- Simulations/simulation_estimator_functions.R
+++ Simulations/simulation_estimator_functions.R
@@ -98,12 +98,12 @@ fit_general <- function(sample)
   ate <- ate$coef[3]
   test <- -Owidth+c<=x & x<=Owidth+c
   write.table(ate,paste0("Results/",dgp,"/ate_sample_",sample,".csv"), row.names = FALSE, col.names = FALSE, sep = ",")
-  time.barddt <- system.time({
-    write.table(fit.barddt(y,x,w,z,test,c),paste0("Results/",dgp,"/barddt_sample_",sample,".csv"), row.names = FALSE, col.names = FALSE, sep = ",")
-  })
-  # time.tbart <- system.time({
-  #   write.table(fit.tbart(y,x,w,z,test,c),paste0("Results/",dgp,"/tbart_sample_",sample,".csv"), row.names = FALSE, col.names = FALSE, sep = ",")
+  # time.barddt <- system.time({
+  #   write.table(fit.barddt(y,x,w,z,test,c),paste0("Results/",dgp,"/barddt_sample_",sample,".csv"), row.names = FALSE, col.names = FALSE, sep = ",")
   # })
+  time.tbart <- system.time({
+    write.table(fit.tbart(y,x,w,z,test,c),paste0("Results/",dgp,"/tbart_sample_",sample,".csv"), row.names = FALSE, col.names = FALSE, sep = ",")
+  })
   # time.sbart <- system.time({
   #   write.table(fit.sbart(y,x,w,z,test,c),paste0("Results/",dgp,"/sbart_sample_",sample,".csv"), row.names = FALSE, col.names = FALSE, sep = ",")
   # })
@@ -115,7 +115,7 @@ fit_general <- function(sample)
   # write.table(c(time.sbart[3],sample),paste0("Time/",dgp,"/sbart.csv"), append=TRUE, row.names = FALSE, col.names = FALSE, sep = ",")
   # write.table(c(time.polynomial[3],sample),paste0("Time/",dgp,"/polynomial.csv"), append=TRUE, row.names = FALSE, col.names = FALSE, sep = ",")
   # write.table(c(time.barddt[3]+time.tbart[3]+time.sbart[3]+time.polynomial[3],sample),paste0("Time/",dgp,"/total_per_sample.csv"), append=TRUE, row.names = FALSE, col.names = FALSE, sep = ",")
-  # # Processing results
+  # Processing results
   # calc.rmse(sample,ate,test)
   # point.est(sample,test)
 }
