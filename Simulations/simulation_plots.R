3f758be19d4dc5140304f2ff86ecc245c6c1aee7
Rafael Alcantara <rafaelcampalc@gmail.com>
2025-02-20 15:18:42 -0600

    Cleaned up simulation_plots file

--- Simulations/simulation_plots.R
+++ Simulations/simulation_plots.R
@@ -1,9 +1,7 @@
-# Owidth <- 0.1
-# c <- 0
-## Boxplots per DGP
-if ("Figures" %in% list.files("Results") == FALSE) dir.create("Results/Figures")
-pdf(paste0("Results/Figures/boxplots.pdf"),width=8,height=28)
-par(mfrow=c(7,2))
+## Check best estimator for all 128 DGPs
+rmse <- matrix(0,128,11)
+colnames(rmse) <- c("k1","k2","k3","k4","k5","p","rho",
+                    "BARDDT","T-BART","S-BART","Polynomial")
 for (i in 1:length(list.files("Results/RMSE")))
 {
   dgp <- list.files("Results/RMSE")[i]
@@ -15,46 +13,23 @@ for (i in 1:length(list.files("Results/RMSE")))
     out[sample,] <- as.matrix(read.table(file.name))
     out[sample,] <- out[sample,]/out[sample,5]
   }
-  out <- out[,1:4]
-  colnames(out) <- c("BARDDT","T-BART","S-BART","Polynomial")
-  ####
-  # pdf(paste0("Results/Figures/boxplots_",dgp,".pdf"))
-  boxplot(out[,-3], main = dgp,frame=FALSE)
-  abline(h=1,col="red")
-  # dev.off()
+  rmse[i,] <- as.numeric(c(strsplit(dgp,"_")[[1]][(1:7)*2],round(colMeans(out)[1:4],3)))
 }
-dev.off()
-## Average fits per W
-pdf(paste0("Results/Figures/fits_.pdf"))
-for (i in 1:length(list.files("Results/Fits")))
-{
-  dgp <- list.files("Results/Fits")[i]
-  file.name <- paste0("Results/Fits/",dgp,"/sample_1.csv")
-  mat <- read.table(file.name)
-  colnames(mat) <- c("W","BARDDT","T-BART","S-BART","Polynomial")
-  ###
-  # pdf(paste0("Results/Figures/fits_",dgp,".pdf"))
-  par(mfrow=c(2,2))
-  matplot(mat[,1],mat[,c(2,3,4)],pch=19,cex=0.6,col=c("black","orange","maroon"),
-          ylab="CATE",xlab=bquote(W[1]),bty="n")
-  matplot(mat[,1],mat[,c(2,3,5)],pch=19,cex=0.6,col=c("black","orange","red"),
-          ylab="CATE",xlab=bquote(W[1]),bty="n")
-  matplot(mat[,1],mat[,c(2,3,6)],pch=19,cex=0.6,col=c("black","orange","pink"),
-          ylab="CATE",xlab=bquote(W[1]),bty="n")
-  plot.new()
-  legend("center",legend=c("BARDDT","T-BART","S-BART","Polynomial"),
-         col=c("orange","maroon","red","pink"),pch=19,cex=0.75,title="Estimator",title.font = 2)
-  mtext(paste(strsplit(dgp,"_")[[1]],collapse=" "), side = 3, line = - 2, outer = TRUE, font = 2)
-  # dev.off()
-}
-dev.off()
+table(apply(rmse[,8:11],1,function(i) which(i==min(i))))
+rmse[apply(rmse[,8:11],1,function(i) which(i==min(i)))!=1,]
 ## Average for each DGP
-rmse.mean <- matrix(0,14,11)
+res.list <- c("k1_1_k2_1_k3_0_k4_0.1_k5_0_p_2_rho_0.5",
+              "k1_1_k2_1_k3_0_k4_0.1_k5_0_p_4_rho_0",
+              "k1_1_k2_1_k3_0_k4_0.1_k5_1_p_2_rho_0",
+              "k1_5_k2_0.25_k3_1_k4_0.5_k5_0_p_4_rho_0.5",
+              "k1_5_k2_0.25_k3_1_k4_0.5_k5_1_p_2_rho_0.5",
+              "k1_5_k2_0.25_k3_1_k4_0.5_k5_1_p_4_rho_0")
+rmse.mean <- matrix(0,6,11)
 colnames(rmse.mean) <- c("k1","k2","k3","k4","k5","p","rho",
                          "BARDDT","T-BART","S-BART","Polynomial")
-for (i in 1:length(list.files("Results/RMSE")))
+for (i in 1:length(res.list))
 {
-  dgp <- list.files("Results/RMSE")[i]
+  dgp <- res.list[i]
   out <- matrix(0,length(list.files(paste0("Results/RMSE/",dgp))),5)
   for (sample in 1:length(list.files(paste0("Results/RMSE/",dgp))))
   {
@@ -81,10 +56,8 @@ print(xtable::xtable(subset(rmse.mean,(k1==1 & k2==1 & k3==0 & k4==0.1) | (k1==5
 ind <- 0
 pdf(paste0("Results/Figures/boxplots.pdf"))
 par(mfrow=c(2,3),mai=c(0.7,0.4,0.4,0.1))
-for (i in 1:length(list.files("Results/RMSE")))
+for (dgp in res.list[1:3])
 {
-  dgp <- list.files("Results/RMSE")[i]
-  if (strsplit(dgp,"_")[[1]][2]!="1" |strsplit(dgp,"_")[[1]][4]!="1" | strsplit(dgp,"_")[[1]][6]!="0" | strsplit(dgp,"_")[[1]][8]!="0.1") next
   ind <- ind+1
   out <- matrix(0,length(list.files(paste0("Results/RMSE/",dgp))),5)
   for (sample in 1:length(list.files(paste0("Results/RMSE/",dgp))))
@@ -96,50 +69,24 @@ for (i in 1:length(list.files("Results/RMSE")))
   }
   out <- out[,1:4]
   colnames(out) <- c("BARDDT","T-BART","S-BART","Polynomial")
-  ####
-  # pdf(paste0("Results/Figures/boxplots_",dgp,".pdf"))
   if (ind==3)
   {
     boxplot(out[,-3], main = "",frame=FALSE,ylim=c(0,1.01),xaxt="n")
     axis(1,1:3,colnames(out)[-3],cex.axis=0.8)
-    # text(x = c(0.75,1.75,2.75),
-    #      y = -0.2,
-    #      labels = colnames(out)[c(1,2,4)],
-    #      xpd = NA,
-    #      ## Rotate the labels by 35 degrees.
-    #      srt = 35,
-    #      cex = 0.7)
   } else if (ind==2)
   {
     boxplot(out[,-3], main = "",frame=FALSE,xaxt="n")
     mtext(bquote("High signal, separable "~mu),line=1,font=2)
     axis(1,1:3,colnames(out)[-3],cex.axis=0.8)
-    # text(x = c(0.75,1.75,2.75),
-    #      y = -0.2,
-    #      labels = colnames(out)[c(1,2,4)],
-    #      xpd = NA,
-    #      ## Rotate the labels by 35 degrees.
-    #      srt = 35,
-    #      cex = 0.7)
   } else
   {
     boxplot(out[,-3], main = "",frame=FALSE,xaxt="n")
     axis(1,1:3,colnames(out)[-3],cex.axis=0.8)
-    # text(x = c(0.75,1.75,2.75),
-    #      y = -0.2,
-    #      labels = colnames(out)[c(1,2,4)],
-    #      xpd = NA,
-    #      ## Rotate the labels by 35 degrees.
-    #      srt = 35,
-    #      cex = 0.7)
   }
   abline(h=1,col="red")
-  # dev.off()
 }
-for (i in 1:length(list.files("Results/RMSE")))
+for (dgp in res.list[4:6])
 {
-  dgp <- list.files("Results/RMSE")[i]
-  if (strsplit(dgp,"_")[[1]][2]!="5" |strsplit(dgp,"_")[[1]][4]!="0.25" | strsplit(dgp,"_")[[1]][6]!="1" | strsplit(dgp,"_")[[1]][8]!="0.5") next
   ind <- ind+1
   out <- matrix(0,length(list.files(paste0("Results/RMSE/",dgp))),5)
   for (sample in 1:length(list.files(paste0("Results/RMSE/",dgp))))
@@ -151,57 +98,38 @@ for (i in 1:length(list.files("Results/RMSE")))
   }
   out <- out[,1:4]
   colnames(out) <- c("BARDDT","T-BART","S-BART","Polynomial")
-  ####
-  # pdf(paste0("Results/Figures/boxplots_",dgp,".pdf"))
   if (ind==5)
   {
     boxplot(out[,-3], main = "",frame=FALSE,xaxt="n")
     mtext(bquote("Low signal, non-separable "~mu),line=1,font=2)
     axis(1,1:3,colnames(out)[-3],cex.axis=0.8)
-    # text(x = c(0.75,1.75,2.75),
-    #      y = -0.2,
-    #      labels = colnames(out)[c(1,2,4)],
-    #      xpd = NA,
-    #      ## Rotate the labels by 35 degrees.
-    #      srt = 35,
-    #      cex = 0.7)
   } else
   {
     boxplot(out[,-3], main = "",frame=FALSE,xaxt="n")
     axis(1,1:3,colnames(out)[-3],cex.axis=0.8)
-    # text(x = c(0.75,1.75,2.75),
-    #      y = -0.2,
-    #      labels = colnames(out)[c(1,2,4)],
-    #      xpd = NA,
-    #      ## Rotate the labels by 35 degrees.
-    #      srt = 35,
-    #      cex = 0.7)
   }
   abline(h=1,col="red")
-  # dev.off()
 }
 dev.off()
 ##
-for (i in 1:length(list.files("Results/Fits")))
+for (dgp in res.list[1:3])
 {
-  dgp <- list.files("Results/Fits")[i]
   if (strsplit(dgp,"_")[[1]][2]!="1" |strsplit(dgp,"_")[[1]][4]!="1" |
       strsplit(dgp,"_")[[1]][6]!="0" | strsplit(dgp,"_")[[1]][8]!="0.1" |
       strsplit(dgp,"_")[[1]][10]!="0" | strsplit(dgp,"_")[[1]][12]!="2" |
       strsplit(dgp,"_")[[1]][14]!="0.5") next
-  file.name <- paste0("Results/Fits/",dgp,"/sample_1.csv")
+  file.name <- paste0("Results/Fits/",dgp,"/cate/sample_1.csv")
   mat <- read.table(file.name)
   colnames(mat) <- c("W","CATE","BARDDT","T-BART","S-BART","Polynomial")
   saveRDS(mat,"easy_fits.rds")
 }
-for (i in 1:length(list.files("Results/Fits")))
+for (dgp in res.list[4:6])
 {
-  dgp <- list.files("Results/Fits")[i]
   if (strsplit(dgp,"_")[[1]][2]!="5" |strsplit(dgp,"_")[[1]][4]!="0.25" |
       strsplit(dgp,"_")[[1]][6]!="1" | strsplit(dgp,"_")[[1]][8]!="0.5" |
       strsplit(dgp,"_")[[1]][10]!="0" | strsplit(dgp,"_")[[1]][12]!="4" |
       strsplit(dgp,"_")[[1]][14]!="0.5") next
-  file.name <- paste0("Results/Fits/",dgp,"/sample_8.csv")
+  file.name <- paste0("Results/Fits/",dgp,"/cate/sample_8.csv")
   mat <- read.table(file.name)
   colnames(mat) <- c("W","CATE","BARDDT","T-BART","S-BART","Polynomial")
   saveRDS(mat,"hard_fits.rds")
