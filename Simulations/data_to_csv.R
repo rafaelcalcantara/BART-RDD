e1465d399b42695503ccaec609de3e502c55de30
Rafael Alcantara <rafaelcampalc@gmail.com>
2025-02-21 00:17:48 -0600

    Added folder and scripts to run sims with Reguly

---
+++ Simulations/data_to_csv.R
@@ -0,0 +1,22 @@
+files <- list.files("Data")
+files <- files[files %in% c("w_2.rds","w_4.rds") == FALSE]
+folders <- gsub(".*dgp_(.+)_sample.*","\\1",files)
+samples <- gsub(".*sample","sample",files)
+samples <- gsub(".rds",".mat",samples)
+## Create data folder for Reguly estimation
+if (dir.exists("Reguly/Data")==FALSE)
+{
+  dir.create("Reguly/Data")
+}
+for (i in 1:length(files))
+{
+  dgp <- files[i]
+  subfolder <- paste0("Reguly/Data/",folders[i])
+  sample <- samples[i]
+  if (dir.exists(subfolder)==FALSE)
+  {
+    dir.create(subfolder)
+  }
+  data <- readRDS(paste0("Data/",dgp))
+  R.matlab::writeMat(paste0(subfolder,"/",sample),a=data)
+}
